258a494e40e22940f8b2cbbfdeaf4a0542f5662a
Script94 <angelpuentes21@gmail.com>
2022-12-28 09:57:40 -0400

    primer commit

---
+++ src/main/java/com/fundamentosplatzi1/springboot/fundamentos1/Fundamentos1Application.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi1.springboot.fundamentos1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fundamentos1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fundamentos1Application.class, args);
+	}
+
+}
